ea11208237004144df23a5d25ccad579b1879ddf
Ben Black <weepingwillowben@reed.edu>
2016-03-16 21:11:33 -0700

    deleted prints

--- source/cachewrapper.c
+++ source/cachewrapper.c
@@ -5,25 +5,18 @@
 
 cache_t create_cache_wrapper(uint64_t maxmem, hash_func hash){
 #ifdef AKOSIK
-    printf("AKOSIK\n");
     return create_cache(maxmem,hash,NULL,NULL);
 #elif defined ALEDGER
-    printf("ALEDGER\n");
     return create_cache(maxmem);
 #elif defined APAN
-    printf("APAN\n");
     return create_cache(maxmem,hash);
 #elif defined BBLACK
-    printf("BBLACK\n");
     return create_cache(maxmem,hash);
 #elif defined JCOSEL
-    printf("JCOSEL\n");
     return create_cache(maxmem,hash);
 #elif defined JHEPWORTH
-    printf("JHEPWORTH\n");
     return create_cache(maxmem);
 #elif defined ZZHONG
-    printf("ZZHONG\n");
     return create_cache(maxmem,hash,NULL);
 #endif
 }
