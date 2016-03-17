e8b036be28bdb6c98f38a494cbb72c42bf2cf9a0
Ben Black <weepingwillowben@reed.edu>
2016-03-16 20:44:13 -0700

    added changes

---
+++ run_test.py
@@ -0,0 +1,15 @@
+import subprocess
+num_of_tests = 3
+execnames = [
+"tests/create_akosik",
+"tests/create_aledger",
+"tests/create_apan",
+"tests/create_bblack",
+"tests/create_jcosel",
+"tests/create_jhepworth",
+"tests/create_zzhong"
+]
+for en in execnames:
+    print(en + " test running:\n")
+    for tn in range(num_of_tests):
+        subprocess.call([en,str(tn)])
--- source/test.c
+++ source/test.c
@@ -1,8 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "cache.h"
 #include "cachewrapper.h"
 
+#define RunTest(testname) {printf(#testname "\n");testname();}
 // void basic_test();
 
 // int main(){
@@ -22,14 +22,37 @@
 
 cache_t create_test();
 
-int main(){
-	printf("TESTING\n\n");
-    create_test();
+int main(int argn,char ** argv){
+	if(argn != 2){
+		printf("needs one argument\n");
+		exit(1);
+	}
+	char * num_str = argv[1];
+	uint64_t test_num = strtoumax(num_str,NULL,10);
+
+	switch (test_num) {
+	case 0:
+		RunTest(create_test);
+		break;
+	case 1:
+		break;
+	case 2:
+		break;
+	case 3:
+		break;
+	case 4:
+		break;
+	default:
+		printf("test not implemented\n");
+		break;
+
+	}
+
     // basic_test();
     return 0;
 }
 cache_t create_test(){
-    cache_t c = create_cache(1000,NULL,NULL);
+    cache_t c = create_cache_wrapper(1000,NULL);
     return c;
 }
 // void basic_test(){
